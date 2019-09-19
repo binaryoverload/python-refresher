09260d43359a0fe39f1154de19dfab9876940061
William Oldham <wegg7250@gmail.com>
2019-09-19 16:40:03 +0100

    Add the ordinal difference calc back, it was removed accidentally

--- abecedarian.py
+++ abecedarian.py
@@ -2,6 +2,7 @@ def isAbecedarian(word):
     lastLetter = None
     for letter in word:
         if lastLetter != None:
+            diff = ord(letter) - (lastLetter)
             if diff == 1 or diff == 0:
                 lastLetter = letter
                 continue
